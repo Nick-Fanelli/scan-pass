e36aff36116d78edb39f67635494f5296cfc8747
Nick Fanelli <nickfanelli5@gmail.com>
2022-02-07 07:45:51 -0500

    fix: display student timestamp in a viewable form

--- src/lav-view/Student.js
+++ src/lav-view/Student.js
@@ -2,11 +2,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimesCircle } from "@fortawesome/free-solid-svg-icons"
 
 export default function Student({ theme, student, processData }) {
+
+    const timestamp = new Date(student.loginTimestamp);
+
+    let hours = timestamp.getHours();
+    let minutes = timestamp.getMinutes();
+
+    const ampm = hours >= 12 ? "pm" : "am";
+
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+
+    minutes = minutes < 10 ? ("0" + minutes) : minutes;
+
+    const displayableTimestamp = hours + ":" + minutes + ampm;
+
     return (
         <tr>
             <td>{student.name}</td>
             <td className="id">{student.id}</td>
-            <td>{student.loginTimestamp}</td>
+            <td>{displayableTimestamp}</td>
             <td className="remove-user-btn" onClick={(e) => { processData(student.id) }}>
                 <FontAwesomeIcon style={{color: theme.text}} icon={faTimesCircle} id="change-location-button" />
             </td>
