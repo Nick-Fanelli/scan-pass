c07dfd0240b0b00f2a54ef96c345cc3236b0096e
Nick Fanelli <nickfanelli5@gmail.com>
2022-04-14 10:44:45 -0400

    fix: only require and config dotenv if in development

--- server/Server.js
+++ server/Server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT || 5000;
